813233dd3eaba682e5235b6f1a55a545a7756104
[dyad] <git@dyad.sh>
2026-03-27 10:25:52 +0200

    Updated src/components/ui/index.tsx

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,5 +1,4 @@
 export { Button } from './Button';
 export { default as Card } from './Card';
 export { Select } from './Select';
-export { Popover } from './Popover';npm run dev
-npm run dev
+export { Popover } from './Popover';
