85350fff131583334544875cab1d6dd5a46e0adc
[dyad] <git@dyad.sh>
2026-03-28 15:32:52 +0200

    [dyad] wrote 1 file(s)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,8 @@
+export { Button } from './Button';
+export { default as Card } from './Card';
+export { Select } from './Select';
+export { Popover } from './Popover';
 export { Switch } from './Switch';
+export { Input } from './Input';
+export { Form } from './Form';
+export { Table } from './Table';
