3651699e3286903550f3bfba2cda5b4c690159f3
[dyad] <git@dyad.sh>
2026-03-29 07:11:22 +0200

    [dyad] Updated main.tsx to wrap the application with VoiceProvider for voice command functionality - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import { VoiceProvider } from "./context/VoiceContext";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <VoiceProvider>
+      <App />
+    </VoiceProvider>
+  </React.StrictMode>
+);
