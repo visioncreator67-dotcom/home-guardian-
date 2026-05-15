4d1b1cb6d431d7f40552c9ab0378e163ae46d64f
[dyad] <git@dyad.sh>
2026-05-15 13:23:36 +0200

    [dyad] Fixed App.tsx syntax error by adding semicolons after each Route element to satisfy OXC plugin requirements - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -17,18 +17,18 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/emergency" element={<EmergencyMode />} />
-        <Route path="/devices" element={<Devices />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/link-provider" element={<LinkProvider />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Index />} />;
+        <Route path="/dashboard" element={<Dashboard />} />;
+        <Route path="/login" element={<Login />} />;
+        <Route path="/signup" element={<Signup />} />;
+        <Route path="/emergency" element={<EmergencyMode />} />;
+        <Route path="/devices" element={<Devices />} />;
+        <Route path="/contacts" element={<Contacts />} />;
+        <Route path="/link-provider" element={<LinkProvider />} />;
+        <Route path="/settings" element={<Settings />} />;
+        <Route path="/forgot-password" element={<ForgotPassword />} />;
+        <Route path="/reset-password" element={<ResetPassword />} />;
+        <Route path="*" element={<NotFound />} />;
       </Routes>
     </Router>
   );
